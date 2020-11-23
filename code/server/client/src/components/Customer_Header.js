6d9bce8612a96bcc80899e3c39519d56397efe2c
Phil Nguyen <philipn7@gmail.com>
2020-11-23 15:22:18 -0500

    Adding app bar

--- code/server/client/src/components/Customer_Header.js
+++ code/server/client/src/components/Customer_Header.js
@@ -1,11 +1,97 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import TheatersIcon from '@material-ui/icons/Theaters';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      textAlign: "left",
+      flexGrow: 1,
+    },
+  }));
 
 function Customer_Header() {
+    const classes = useStyles();
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+  
+    const handleChange = (event) => {
+      setAuth(event.target.checked);
+    };
+  
+    const handleMenu = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
     return (
-        <div>
-            
-        </div>
-    )
-}
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <TheatersIcon></TheatersIcon>
+            <Typography variant="h6" className={classes.title}>
+              VideoCo
+            </Typography>
+
+
+            <Button color="inherit">Search</Button>
+            <Button color="inherit">Cart</Button>
+
+            {auth && (
+              <div>
+                <IconButton
+                  aria-label="account of current user."
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>View Profile</MenuItem>
+                  <MenuItem onClick={handleClose}>View History</MenuItem>
+                  <br></br>
+                  <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 
 export default Customer_Header
