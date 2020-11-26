84af7f497e1d81b4f80aaa9a131ccfc8e36c24f8
Huan Ho (Bruce) <37372806+bruceho293@users.noreply.github.com>
2020-11-26 15:14:27 -0500

    Update the Result and Search

--- code/server/client/src/components/Results.js
+++ code/server/client/src/components/Results.js
@@ -1,9 +1,101 @@
 import React from 'react'
+import { makeStyles } from "@material-ui/core/styles"
+import Container from "@material-ui/core/Container"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import Typography from "@material-ui/core/Typography"
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea' 
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+
+const useStyles = makeStyles(()=> ({
+    splitScreen: {
+        display: "flex",
+        flexDirection: "row",
+    },
+    
+    splitPage1: {
+        width: '60%',
+    },
+    
+    splitPage2: {
+        width: '40%'
+    },
+
+    gridList: {
+        height: 600,
+    }
+}));
 
 function Results() {
+
+    const classes = useStyles();
+
+    const data = [
+        {"id":  1, "title": "Apple", "Price": 1},
+        {"id":  2, "title": "Apple", "Price": 2},
+        {"id":  3, "title": "Apple", "Price": 3},
+        {"id":  4, "title": "Apple", "Price": 4},
+        {"id":  5, "title": "Apple", "Price": 5},
+        {"id":  6, "title": "Apple", "Price": 18},
+        {"id":  7, "title": "Apple", "Price": 13},
+        {"id":  8, "title": "Apple", "Price": 16},
+        {"id":  9, "title": "Apple", "Price": 12},
+        {"id": 10, "title": "Apple", "Price": 11},
+        {"id": 11, "title": "Apple", "Price": 1},
+        {"id": 12, "title": "Apple", "Price": 2},
+        {"id": 13, "title": "Apple", "Price": 3},
+        {"id": 14, "title": "Apple", "Price": 4},
+        {"id": 15, "title": "Apple", "Price": 5},
+        {"id": 16, "title": "Apple", "Price": 18},
+        {"id": 17, "title": "Apple", "Price": 13},
+        {"id": 18, "title": "Apple", "Price": 16},
+        {"id": 19, "title": "Apple", "Price": 12},
+        {"id": 20, "title": "Apple", "Price": 11}
+    ];
+
     return (
-        <div>
-            
+        <div className={classes.splitScreen}>
+        <React.Fragment>
+            <CssBaseline />
+            <Container className={classes.splitPage1}>
+                <GridList cellHeight={200} cellWidth={50} className={classes.gridList}>
+                {data.map((d) => (
+                    <GridListTile key={d.id}>
+                        <Card>
+                            <CardActionArea>
+                             <CardMedia
+                                component="img"
+                                alt="Video Image"
+                                height="120"
+                                width="50"
+                                source={{
+                                    url: 'https://reactnative.dev/img/tiny_logo.png',
+                                }}
+                                title="Video"
+                            />
+                            <CardContent>
+                                <Typography variant="h5" component="h2">
+                                    {d.title}
+                                </Typography>
+                            </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </GridListTile>
+                ))}
+                </GridList>
+            </Container>
+         </React.Fragment>  
+        <React.Fragment>
+            <CssBaseline />
+            <Container className={classes.splitPage2}>
+                <Typography paragraph>
+                    Hello World 2
+                </Typography>
+            </Container>
+         </React.Fragment>
         </div>
     )
 }
--- code/server/client/src/components/Search.js
+++ code/server/client/src/components/Search.js
@@ -3,10 +3,10 @@ import { makeStyles } from "@material-ui/core/styles"
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
 import FormControl from "@material-ui/core/FormControl"
-import FormLabel from "@material-ui/core/FormLabel"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Radio from "@material-ui/core/Radio"
 import RadioGroup from "@material-ui/core/RadioGroup"
+
 
 const useStyles = makeStyles((theme) => ({
 
@@ -19,12 +19,6 @@ const useStyles = makeStyles((theme) => ({
         width: "80%",
         position: "relative",
     },
-
-    textField: {
-      width: "40%",
-      position: "relative",  
-    },
-
 
     button: {
         margin: theme.spacing(1, 1, 0, 0),
@@ -44,15 +38,20 @@ function Search() {
         <div>
             <form className={classes.searchForm}>
                 <FormControl component="fieldset" className={classes.formControl}>
-                    <FormLabel component="legend">VideoCo Search</FormLabel>
-                    <TextField placeholder={"Enter the " + value + " of the video"} className="classes.textField"></TextField>
+                    <TextField 
+                        id="outlined-textarea"
+                        label="VideoCo Search"
+                        placeholder={"Enter the " + value + " of the video"} 
+                        multiline
+                        variant="outlined"
+                    >    
+                    </TextField>
                     <Button variant="contained" color="primary" className={classes.button}>
                         Seacrh
                     </Button>
                     <RadioGroup aria-label="criteria" name="criteria" value={value} onChange={handleChange}>
                         <FormControlLabel value="title" control={<Radio />} label="Title" />
                         <FormControlLabel value="director" control={<Radio />} label="Director" />
-                        <FormControlLabel value="price" control={<Radio />} label="Price" />
                     </RadioGroup>
                 </FormControl>
             </form>
